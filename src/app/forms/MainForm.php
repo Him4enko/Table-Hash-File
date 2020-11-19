c8b6ad8c8addd1205f323da149dc45fd8c78c88f
Евгений Химченко <him4enko@hotmail.com>
2020-11-19 20:54:13 +0300

    #1 Init. table_hash
    Обновление Table_Hash_File | DN 16.7.0
    # Теперь папки и файлы с отсутствующим  хешем или возвращающие значение NULL, исключаются из списка.

---
+++ src/app/forms/MainForm.php
@@ -0,0 +1,39 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+use php\io\File;
+use php\lang\Thread;
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event button.action 
+     */
+    function doButtonAction(UXEvent $e = null)
+    {    
+    
+         $dir = $this->dirChooser->execute();
+            $list = $dir->findFiles();
+            $count = count($list);
+            $this->label->text = $count;
+            $thread = new Thread(function () use ($list){
+            foreach ($list as $val){
+                    $val_hash = $val->hash('SHA-256');
+                    if($val_hash == NULL){
+                    
+                    }else{
+                    $this->table->items->add(['Path' => $val, 'Hash' => $val_hash]);
+                    }
+                }
+                
+        });
+            $thread->start();
+         
+    }
+   
+
+
+}
+
